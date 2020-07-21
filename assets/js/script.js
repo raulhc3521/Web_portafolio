2c0d8458fd2496330255323571f38a584cdd5f71
raulhc3521 <raulhc3521@users.noreply.github.com>
2020-07-21 19:44:32 -0400

    animaciones js

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function rotate() {
+    var avatar = document.querySelector('.descripcion__avatar');
+    var scrollup = document.querySelector('.scroll');
+    var navbar = document.querySelector('.navbar');
+
+    if(this.scrollY > 10){
+        avatar.classList.add("rotate_avatar");
+        scrollup.classList.add("rotate_scrollup");
+        navbar.classList.add("nav_scroll");
+    } else {
+        avatar.classList.remove("rotate_avatar");
+        scrollup.classList.remove("rotate_scrollup");
+        navbar.classList.remove("nav_scroll");
+    }
+}
+
+window.addEventListener("scroll", rotate);
+
