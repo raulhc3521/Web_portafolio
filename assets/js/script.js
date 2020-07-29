d14b23e51a21ffb9bcd5c3167af316ce7ee87a12
raulhc3521 <raulhc3521@users.noreply.github.com>
2020-07-29 14:35:29 -0400

    modal agregado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Función de rotacion
+//Función de rotación
 function rotate() {
     var avatar = document.querySelector('.descripcion__avatar');
     var scrollup = document.querySelector('.scroll');
@@ -17,7 +17,32 @@ function rotate() {
 window.addEventListener("scroll", rotate);
 
 
-//Función uncheck //
+//Función uncheck
 function uncheck() {
     document.getElementById("nav__desplegable").checked = false;
 }
+
+//Funciones Modales
+var modal1 = document.querySelector('.modal-1');
+var modal2 = document.querySelector('.modal-2');
+var modal3 = document.querySelector('.modal-3');
+var modal4 = document.querySelector('.modal-4');
+
+
+function open_modal1() {
+    modal1.style.visibility = "visible";
+    modal1.style.opacity = 0;
+    setTimeout(() => {
+        modal1.style.opacity = 1;
+    }, this.animationDelay + 20); 
+}
+
+
+function close_modal() {
+    modal1.style.opacity = 1;
+    setTimeout(() => {
+        modal1.style.opacity = 0;
+    }, this.animationDelay + 20);
+    modal1.style.visibility = "hidden";    
+}
+
