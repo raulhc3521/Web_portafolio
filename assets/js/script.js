68d2b5b686b21fd6eedbad469916b5545bd36d03
raulhc3521 <raulhc3521@users.noreply.github.com>
2020-07-31 15:43:28 -0400

    correcciones responsividad

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,74 +16,102 @@ function rotate() {
 }
 window.addEventListener("scroll", rotate);
 
-
 //Función uncheck
 function uncheck() {
     document.getElementById("nav__desplegable").checked = false;
 }
 
 //Funciones Modales
-var modal1 = document.querySelector('.modal-1');
-var modal2 = document.querySelector('.modal-2');
-var modal3 = document.querySelector('.modal-3');
-var modal4 = document.querySelector('.modal-4');
-var body = document.querySelector('body');
-
-
 function open_modal1() {
-    modal1.style.visibility = "visible";
-    modal1.style.opacity = 0;
+    var modal1 = document.querySelector('.modal-1');
+    var body = document.querySelector('body');
+    var modal = document.querySelector('.modal--overflow1');
+
+    modal1.style.cssText = "opacity: 0; visibility: visible;";
     setTimeout(() => {
         modal1.style.opacity = 1;
     }, this.animationDelay + 20);
+    if (screen.width > 1200) {
+        modal.style.margin = "10px 10px 10px 4px";
+        body.style.margin = "0 6px 0 0"; 
+    }    
     body.style.overflow = "hidden";
-    body.style.margin = "0 6px 0 0"; 
+    
+    
 }
 function open_modal2() {
-    modal2.style.visibility = "visible";
-    modal2.style.opacity = 0;
+    var modal2 = document.querySelector('.modal-2');
+    var body = document.querySelector('body');
+    var modal = document.querySelector('.modal--overflow2');
+
+    modal2.style.cssText = "opacity: 0; visibility: visible;";
     setTimeout(() => {
         modal2.style.opacity = 1;
     }, this.animationDelay + 20);
-    body.style.overflow = "hidden";
-    body.style.margin = "0 6px 0 0";  
+    if (screen.width > 1200) {
+        modal.style.margin = "10px 10px 10px 4px";
+        body.style.margin = "0 6px 0 0"; 
+    }    
+    body.style.overflow = "hidden"; 
 }
 function open_modal3() {
-    modal3.style.visibility = "visible";
-    modal3.style.opacity = 0;
+    var modal3 = document.querySelector('.modal-3');
+    var body = document.querySelector('body');
+    var modal = document.querySelector('.modal--overflow3');
+
+    modal3.style.cssText = "opacity: 0; visibility: visible;";
     setTimeout(() => {
         modal3.style.opacity = 1;
     }, this.animationDelay + 20);
+    if (screen.width > 1200) {
+        modal.style.margin = "10px 10px 10px 4px";
+        body.style.margin = "0 6px 0 0"; 
+    }    
     body.style.overflow = "hidden";
-    body.style.margin = "0 6px 0 0"; 
 }
 function open_modal4() {
-    modal4.style.visibility = "visible";
-    modal4.style.opacity = 0;
+    var modal4 = document.querySelector('.modal-4');
+    var body = document.querySelector('body');
+    var modal = document.querySelector('.modal--overflow4');
+
+    modal4.style.cssText = "opacity: 0; visibility: visible;";
     setTimeout(() => {
         modal4.style.opacity = 1;
     }, this.animationDelay + 20);
+    if (screen.width > 1200) {
+        modal.style.margin = "10px 10px 10px 4px";
+        body.style.margin = "0 6px 0 0"; 
+    }    
     body.style.overflow = "hidden";
-    body.style.margin = "0 6px 0 0";  
 }
 
-
 function close_modal() {
-    body.style.overflow = "auto";
-    body.style.margin = "0 0 0 0"; 
+    var modal1 = document.querySelector('.modal-1');
+    var modal2 = document.querySelector('.modal-2');
+    var modal3 = document.querySelector('.modal-3');
+    var modal4 = document.querySelector('.modal-4');
+    var body = document.querySelector('body');
+    var modalo1 = document.querySelector('.modal--overflow1');
+    var modalo2 = document.querySelector('.modal--overflow2');
+    var modalo3 = document.querySelector('.modal--overflow3');
+    var modalo4 = document.querySelector('.modal--overflow4');
+
+    body.style.overflow = "auto"; 
+    if (screen.width > 1200) {
+        modalo1.style.margin = "10px 10px 10px 10px";
+        modalo2.style.margin = "10px 10px 10px 10px";
+        modalo3.style.margin = "10px 10px 10px 10px";
+        modalo4.style.margin = "10px 10px 10px 10px";
+        body.style.margin = "0 0px 0 0"; 
+    }
     modal1.style.opacity = 1;
     modal2.style.opacity = 1;
     modal3.style.opacity = 1;
     modal4.style.opacity = 1;
     setTimeout(() => {
-        modal1.style.opacity = 0;
-        modal2.style.opacity = 0;
-        modal3.style.opacity = 0;
-        modal4.style.opacity = 0;
+        modal1.style.cssText = "opacity: 0; visibility: hidden;";
+        modal2.style.cssText = "opacity: 0; visibility: hidden;";
+        modal3.style.cssText = "opacity: 0; visibility: hidden;";
+        modal4.style.cssText = "opacity: 0; visibility: hidden;";
     }, this.animationDelay + 20);
-    modal1.style.visibility = "hidden";
-    modal2.style.visibility = "hidden";
-    modal3.style.visibility = "hidden";
-    modal4.style.visibility = "hidden";
 }
-
